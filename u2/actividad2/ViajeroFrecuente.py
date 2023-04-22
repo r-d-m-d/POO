cb671ad4fba5424cad1a9270e69040679dfc2a08
Ricardo Martin <ricardo.d.martin@gmail.com>
2023-04-22 16:52:32 -0300

    Se crea la clase ViajeroFrecuente

---
+++ u2/actividad2/ViajeroFrecuente.py
@@ -0,0 +1,26 @@
+class ViajeroFrecuente():
+    __num_viajero = 0
+    __dni = ""
+    __nombre = ""
+    __apellido = ""
+    __millas_acum = 0
+
+    def __init__(self, numv, dni, nombre, apellido, millas):
+        self.__num_viajero = numv
+        self.__dni = dni
+        self.__nombre = nombre
+        self.__apellido = apellido
+        self.__millas_acum = millas
+
+    def cantidadTotalDeMillas(self):
+        return self.__millas_acum
+
+    def acumularMillas(self, mill):
+        return self.__millas_acum+mill
+
+    def canjearMillas(self, millas_canje):
+        if self.__millas_acum >= millas_canje:
+            self.__millas_acum -= millas_canje
+        return self.__millas_acum
+
+
