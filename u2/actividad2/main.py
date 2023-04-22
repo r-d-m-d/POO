a8adc1290b95342c8563638e93e8e85b02c1786b
Ricardo Martin <ricardo.d.martin@gmail.com>
2023-04-22 16:54:53 -0300

    Se bosqueja un menu en main.py

---
+++ u2/actividad2/main.py
@@ -0,0 +1,20 @@
+
+
+
+
+if __name__ == "__main__":
+    print("a- Consultar cantidad de millas")
+    print("b- Acumular Millas")
+    print("c- Canjear Millas")
+    opc=input("ingrese opcion: ")
+    if opc == "a":
+        pass
+    elif opc == "b":
+        pass
+    elif opc == "c":
+        pass
+    elif opc == "s":
+        pass
+    else:
+        pass
+
