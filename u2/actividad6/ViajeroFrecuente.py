1c1de4058215f3e6592f65f604f728201a9f1d8f
Ricardo Martin <ricardo.d.martin@gmail.com>
2023-05-03 12:40:34 -0300

    Se agregan los operadores __lt__, __add__, __sub__ como se detalla en el enunciado

---
+++ u2/actividad6/ViajeroFrecuente.py
@@ -0,0 +1,43 @@
+class ViajeroFrecuente():
+    __num_viajero = 0
+    __dni = ""
+    __nombre = ""
+    __apellido = ""
+    __millas_acum = 0
+
+    def __init__(self, numv, dni, nombre, apellido, millas):
+        self.__num_viajero = numv
+        self.__dni = dni
+        self.__nombre = nombre
+        self.__apellido = apellido
+        self.__millas_acum = millas
+
+    def cantidadTotalDeMillas(self):
+        return self.__millas_acum
+
+    def acumularMillas(self, mill):
+        return self.__millas_acum+mill
+
+    def puedeCanjear(self, millas):
+        return self.__millas_acum >= millas
+
+    def canjearMillas(self, millas_canje):
+        if self.puedeCanjear(millas_canje):
+            self.__millas_acum -= millas_canje
+        return self.__millas_acum
+
+    def tieneNumero(self, numero):
+        return numero == self.__num_viajero
+
+    def __gt__(self, otro):
+        return self.__millas_acum > otro.__millas_acum
+
+    def __add__(self, millas):
+        if type(millas) == type(int):
+            self.acumularMillas(millas)
+        return self
+
+    def __sub__(self, millas):
+        if type(millas) == type(int):
+            self.canjearMillas(millas)
+        return self
