9db821dbc6825768965b5498da2e2bbb3a2c3e60
Ricardo Martin <ricardo.d.martin@gmail.com>
2023-06-05 12:38:33 -0300

    Cambio la lista de vehiculos por un manejador

--- u3/ej6/main.py
+++ u3/ej6/main.py
@@ -1,19 +1,22 @@
-import json
+import datetime
 
-from Lista import Lista
-from Nodo import Nodo
-
-from Vehiculo import Vehiculo
 from Nuevos import Nuevos
 from Usados import Usado
+from ManejaVehiculos import ManejaVehiculos
+
 
 class Main:
 
     def __init__(self):
-        self.__lista_vehiculos = Lista()
+        self.__mv = ManejaVehiculos()
+
+    def cargarJson(self, fn):
+        self.__mv.cargarArchivo(fn)
 
     def main(self):
-       self.menuPrincipal() 
+        opc = ""
+        while opc.lower() != 's':
+            opc = self.menuPrincipal()
 
     def menuPrincipal(self):
         opciones =[
@@ -26,13 +29,14 @@ class Main:
                 'Almacenar los objetos de la colección Lista en el archivo “vehiculos.json”.']
         for n, t in enumerate(opciones):
             print(f"{n+1}) {t}")
+        print("s) Salir.")
         opc = input("Ingrese una opcion: ")
         if opc == "1":
-            pass
+            self.insertarVehiculo()
         elif opc == "2":
-            pass
+            self.agregarVehiculo()
         elif opc == "3":
-            pass
+            self.punto3()
         elif opc == "4":
             self.punto4()
         elif opc == "5":
@@ -45,6 +49,7 @@ class Main:
             pass
         else:
             print("Opcion incorrecta")
+        return opc
     def crearNuevo(self):
         modelo = input("ingrese el modelo: ")
         np = int(input("ingrese el numero de puertas: "))
@@ -64,30 +69,6 @@ class Main:
         kilom = int(input("Kilometraje: "))
         return Usado(modelo, np, color, precio, marca, patente, anio, kilom)
 
-
-    def cargarJson(self, jsonFn):
-        with open(jsonFn) as fp:
-            data = json.load(fp)
-        for vehiculo_data in data:
-            modelo = vehiculo_data["modelo"]
-            puertas = vehiculo_data["cantidad_puertas"]
-            color = vehiculo_data["color"]
-            precio = vehiculo_data["precio_base"]
-            vehiculo = None
-            if vehiculo_data["__class__"] == "Nuevo":
-                version = vehiculo_data["version"]
-                vehiculo = Nuevos(modelo, puertas, color, precio, version)
-            elif vehiculo_data["__class__"] == "Usado":
-                marca = vehiculo_data["marca"]
-                patente = vehiculo_data["patente"]
-                anio = vehiculo_data["anio"]
-                kilometraje = vehiculo_data["kilometraje"]
-                vehiculo = Usado(modelo, puertas, color, precio, marca,
-                                 patente, anio, kilometraje)
-            if vehiculo is not None:
-                nodo = Nodo(vehiculo)
-                self.__lista_vehiculos.agregarElemento(nodo)
-
     def insertarVehiculo(self):
         v = None
         pos = int(input("ingrese la posicion a insertar el vehiculo: "))
@@ -99,8 +80,7 @@ class Main:
         elif opc == "2":
             v = self.crearUsado()
         if v is not None:
-            nv = Nodo(v)
-            self.__lista_vehiculos.insertarElemento(nv, pos)
+            self.__mv.insertarVehiculo(v, pos)
 
     def agregarVehiculo(self):
         v = None
@@ -112,41 +92,43 @@ class Main:
         elif opc == "2":
             v = self.crearUsado()
         if v is not None:
-            self.__lista_vehiculos.agregarVehiculo(v)
+            self.__mv.agregarVehiculo(v)
 
     def punto3(self):
         pos = int(input("Ingrese una posicion"))
-        e = self.__lista_vehiculos.obtenerElemento(pos)
-        print(e.tipo())
+        e = self.__mv.punto3(pos)
+        print(e.getDato().tipo())
 
     def punto4(self):
         pat = input("Ingrese Pantente")
-        i = iter(self.__lista_vehiculos)
-        v = next(i, False)
-        while isinstance(v, Vehiculo):
-            v = next(i, False)
-        if isinstance(v, Vehiculo):
-            print(v.tipo())
+        v = self.__mv.buscarPatente(pat)
+        if isinstance(v, Usado):
+            np = int(input("Ingrese un nuevo precio: "))
+            v.precio(np)
+            print(f"El nuevo precio de venta: {v.importeVenta()}")
         else:
             print("Patente no encontrada")
 
     def punto5(self):
-        m = self.__lista_vehiculos.obtenerElemento(0).getDato()
-        for v in self.__lista_vehiculos:
-            if v.importeVenta() < m.importeVenta():
-                m = v
+        m = self.__mv.vehiculoMasEconomico()
         print(f"El vehiculo mas economico: {m}")
 
     def punto6(self):
-        for v in self.__lista_vehiculos:
-            print(v)
+        print(self.__mv.listarVehiculos())
+
     def punto7(self):
-        for v in self.__lista_vehiculos:
-            print(v.toJson())
+        filename = f"vehiculos-{datetime.date.today()}.json"
+        ifn = input(f"Ingrese un nombre de archivo({filename}): ")
+        if ifn == "":
+            ifn = filename
+        self.__mv.guardarVehiculos(ifn)
 
-jsonFn = "vehiculos.json"
 
+jsonFn = f"vehiculos-{datetime.date.today()}.json"
 if __name__ == "__main__":
     m = Main()
-    m.cargarJson(jsonFn)
+    j = input(f"Ingrese el nombre del json({jsonFn}): ")
+    if j == "":
+        j = jsonFn
+    m.cargarJson(j)
     m.main()
