f2c459cb2f8f69756091e36e4b240394dfd8b63a
Ricardo Martin <ricardo.d.martin@gmail.com>
2024-07-16 17:58:45 -0300

    Cambio de api para obtener el precio del dolar y su manejo.

--- u4/ej3/ObtenerDolar.py
+++ u4/ej3/ObtenerDolar.py
@@ -1,25 +1,44 @@
 import requests
+import time
+
 
 class APIDeDolar:
     __url: str
-    __precio: float
+    __datos: float
+    __interprete: object
+    __reintentos: int
+    __espera: int
 
-    def __init__(self, url):
+    def __init__(self, url, interprete):
         self.__url = url
-        self.__precio = self.obtenerDolar()
+        self.__interprete = interprete
+        self.__reintentos = 3
+        self.__espera = 1  # Tiempo de espera inicial
+        self.__datos = None
+        self.obtenerDatos()
 
-    def obtenerDolar(self):
-        respose = requests.get(self.__url)
-        jd = respose.json() if respose and respose.status_code == 200 else None
-        tam = len(jd)
-        i = 0
-        precio_venta = 0
-        while i < tam and jd[i]['casa']['nombre'] != 'Oficial':
-            i += 1
+    def obtenerDatos(self):
+        reintentos = self.__reintentos
+        espera = self.__espera
+        response = None
+        while reintentos > 0:
+            try:
+                response = requests.get(self.__url)
+                response.raise_for_status()  # Levantar un error http
+            except requests.RequestException as e:
+                print(f"Request failed: {e}")
+                if reintentos > 0:
+                    print(f"Retrying in {espera} seconds...")
+                    time.sleep(espera)
+                    espera *= 2  # Tiempo de espera exponecial
+                else:
+                    print("Max retries exceeded. Exiting.")
+                    self.__datos = None
+            else:
+                self.__datos = self.__interprete(response)
+                reintentos = 0
+            reintentos -= 1
 
-        if i < tam and jd[i]['casa']['nombre'] == 'Oficial':
-            precio_venta = float(jd[i]['casa']['venta'].replace(',', '.'))
-        return precio_venta
-
-    def precio(self):
-        return self.__precio
+    @property
+    def datos(self):
+        return self.__datos
--- u4/ej3/main.py
+++ u4/ej3/main.py
@@ -10,12 +10,19 @@ from tkinter import ttk, messagebox
 
 from ObtenerDolar import APIDeDolar
 
+
+def interprete_datos(rjd):
+    return rjd.json()['venta']
+
+
 class Aplicacion():
-    __ventana=None
-    __pulgadas=None
-    __centimetros=None
+    __ventana = None
+    __pulgadas = None
+    __centimetros = None
+
     def __init__(self):
-        self.__precioVenta = APIDeDolar("https://www.dolarsi.com/api/api.php?type=dolar").precio()
+        self.__precioVenta = APIDeDolar(
+            "https://dolarapi.com/v1/dolares/oficial", interprete_datos).datos
         self.__ventana = Tk()
         self.__ventana.geometry('290x115')
         self.__ventana.title('Conversor de moneda')
@@ -28,12 +35,16 @@ class Aplicacion():
         self.__dolares = StringVar()
         self.__pesos = StringVar()
         self.__dolares.trace('w', self.calcular)
-        self.dolaresEntry = ttk.Entry(mainframe, width=7, textvariable=self.__dolares)
+        self.dolaresEntry = ttk.Entry(
+            mainframe, width=7, textvariable=self.__dolares)
         self.dolaresEntry.grid(column=2, row=1, sticky=(W, E))
-        ttk.Label(mainframe, textvariable=self.__pesos).grid(column=2, row=2, sticky=(W, E))
-        ttk.Button(mainframe, text='Salir', command=self.__ventana.destroy).grid(column=3, row=3, sticky=W)
+        ttk.Label(mainframe, textvariable=self.__pesos).grid(
+            column=2, row=2, sticky=(W, E))
+        ttk.Button(mainframe, text='Salir', command=self.__ventana.destroy).grid(
+            column=3, row=3, sticky=W)
         ttk.Label(mainframe, text="dolares").grid(column=3, row=1, sticky=W)
-        ttk.Label(mainframe, text="es equivalente a").grid(column=1, row=2, sticky=E)
+        ttk.Label(mainframe, text="es equivalente a").grid(
+            column=1, row=2, sticky=E)
         ttk.Label(mainframe, text="pesos").grid(column=3, row=2, sticky=W)
         for child in mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
@@ -41,9 +52,9 @@ class Aplicacion():
         self.__ventana.mainloop()
 
     def calcular(self, *args):
-        if self.dolaresEntry.get()!='':
+        if self.dolaresEntry.get() != '':
             try:
-                valor=float(self.dolaresEntry.get())
+                valor = float(self.dolaresEntry.get())
                 self.__pesos.set(f'{self.__precioVenta*valor:.2f}')
             except ValueError:
                 messagebox.showerror(title='Error de tipo',
@@ -56,5 +67,7 @@ class Aplicacion():
 
 def testAPP():
     mi_app = Aplicacion()
+
+
 if __name__ == '__main__':
     testAPP()
